c016e71684e5ec1a22446ae5b9ec5d4e8bb7cc76
mouxiaohui <18367652239@163.com>
2022-05-30 15:36:53 +0800

    Maximum Subarray

--- easy/Maximum_Subarray/Maximum_Subarray.go
+++ easy/Maximum_Subarray/Maximum_Subarray.go
@@ -27,7 +27,7 @@ package maximum_subarray
 */
 
 func maxSubArray(nums []int) int {
-	max := nums[0] // -2
+	max := nums[0]
 	for i := 1; i < len(nums); i++ {
 		if nums[i]+nums[i-1] > nums[i] {
 			nums[i] += nums[i-1]
